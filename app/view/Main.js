9a9f3ddb9b31ac087af2c0d0e82285383fbed0a9
Adam Ellison <adotellison@gmail.com>
2012-10-07 15:18:39 +0100

    Fixing Main.js and adding Sass

--- app/view/Main.js
+++ app/view/Main.js
@@ -8,19 +8,21 @@ Ext.define("Colophone.view.Main", {
             {
                 xtype:'panel',
                 html:'logo',
-                style: 'background:#CCC;',
+                cls: 'header',
                 docked: 'top',
                 //flex: 1
             },
             {
                 xtype:'panel',
                 html:'Scrolly stuff',
+                cls: 'content',
                 style:'background:#FF0000;',
                 flex: 1
             },
             {
                 xtype:'panel',
                 style: 'background:#FFF000;',
+                cls: 'footer',
                 docked:'bottom',
                 layout:'hbox',
                 items: [
